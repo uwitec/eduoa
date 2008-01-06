1a50c46a4fc7f0fb684de16a3cf78520fb80832e
longben <longben@cc020657-dd42-0410-9910-adba8de85fba>
2008-01-06 17:37:44 +0000

    系统初始化表和业务表

--- src/sql/eduoa.sql
+++ src/sql/eduoa.sql
@@ -9,6 +9,31 @@
 /*  在使用0和1表示状态的时候，如无特殊说明0始终表示无效，1始终表示有效。
 /*********************************************************************************************
 
+/* 行政区划 */
+create table regions(
+  id               int(11)       not null auto_increment comment '主键',
+  region_name      varchar(30)   not null                comment '行政地区名称',
+  maximum          int(6)        not null default 0      comment '最大记录数',
+  flag             int(1)        not null                comment '有效标志',
+  primary key (id)
+) engine=MyISAM default charset=utf8 comment='行政区划';
+
+
+/* 角色表 */
+create table roles (
+  id                   int(10)         not null auto_increment comment '主键',
+  role_name            varchar(200)    not null                comment '角色名称',
+  type_id              int(1)          not null default 1      comment '类型',
+  hierarchy            int(1)                                  comment '角色等级',
+  father_id            int(1)                                  comment '父亲角色',
+  valid_from           date                                    comment '生效日期',
+  valid_thru           date                                    comment '终止日期',
+  flag                 int(1)          not null default 1      comment '有效标志(1: 有效; 0: 无效)',
+  memo                 varchar(2000)                           comment '备注',
+  primary key  (id)
+) engine=MyISAM default charset=utf8 comment='角色';
+
+
 /* 栏目表 */
 create table modules (
   id              int(10)         not null auto_increment comment '主键',
@@ -34,4 +59,40 @@ create table modules (
   flag            int(1)          default 1               comment '有效标志',
   primary key  (id)
  ) engine=MyISAM default charset=utf8 comment='栏目表';
+
+
+/* 会员等级 */
+create table member_grades(
+  id               int(2)        not null auto_increment comment '主键',
+  grade_name       varchar(20)   not null                comment '会员等级名称',
+  member_per       decimal(2,2)  not null default 0      comment '会员',
+  ws_per           decimal(2,2)  not null default 0      comment '会员',
+  primary key (id)
+) engine=MyISAM default charset=utf8 comment='会员等级';
+
+
+/* 会员基本信息表 */
+create table users(
+  id               int(8)        not null                comment '主键，值同members主键值',
+  login_name       varchar(15)   not null  default ''    comment '用户登录名',
+  password         varchar(32)   not null  default ''    comment '用户口令',  
+  user_name        varchar(20)   not null                comment '真实姓名',
+  sex              int(1)        not null  default 1     comment '性别: 1:男 0:女',
+  member_no        varchar(25)   not null  default ''    comment '省编号+身份证号码成为会员编号',
+  cert_int         varchar(18)   not null  default ''    comment '身份证号码',
+  referees         int(8)                                comment '推荐人',
+  member_grades_id int(2)        not null default 1      comment '会员等级，默认为普通会员',
+  region_id        int(11)                               comment '会员所属地区',
+  telephone        varchar(30)                           comment '联系电话',
+  mobile           varchar(11)                           comment '移动电话',
+  office_phone     varchar(30)                           comment '办公电话',
+  home_phone       varchar(30)                           comment '家庭电话',
+  email            varchar(30)                           comment '电子邮件',
+  bank_accounts    varchar(30) DEFAULT '邮政储蓄'	 comment '开户银行',
+  accounts         varchar(20)                           comment '银行帐号',
+  created          timestamp                             comment '会员创建日期',
+  role_id          int(6)        default 1               comment '用户角色',       
+  flag             int(1)        not null default 1      comment '会员状态',
+  primary key (id)
+) engine=MyISAM default charset=utf8 comment='会员基本信息';
 

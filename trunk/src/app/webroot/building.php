2eb3808d310b3812d6d4248e7046748e562c63ee
cloudlin <cloudlin@cc020657-dd42-0410-9910-adba8de85fba>
2008-01-14 03:04:06 +0000

    code 问题

--- trunk/src/app/webroot/building.php
+++ trunk/src/app/webroot/building.php
@@ -1,1 +1,9 @@
+<html>
+<head>
+<meta http-equiv="Content-Language" content="zh-cn">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
 <br><br><br><br><div align="center">栏目建设中……</div>
+</body>
+</html>

25ab08683fc9d20c60b63ed27fb32e52b9941653
longben <longben@cc020657-dd42-0410-9910-adba8de85fba>
2008-01-14 16:58:22 +0000

    调整资产借出和归还

--- src/sql/资产管理表.sql
+++ src/sql/资产管理表.sql
@@ -3,8 +3,8 @@
 -- --------------------------------------
 create table asset_types (
   id                    int(10)       not null auto_increment comment '资产类型编号',
-  type_name             varchar(200)                          COMMENT '名称',
-  type_code             varchar(200)                          COMMENT '编码',
+  type_name             varchar(200)                          comment '名称',
+  type_code             varchar(200)                          comment '编码',
   primary key (id)
 )engine=MyISAM default charset=utf8 comment='资产类型';
 
@@ -12,7 +12,7 @@ create table asset_types (
 -- --------------------------------------
 -- 资产状态表
 -- --------------------------------------
-create table asset_status (
+create table asset_statuses (
   id                    int(10)       not null auto_increment comment '资产状态编号',
   status_name           varchar(200)                          comment '名称',
   status_code           varchar(200)                          comment '编码',
@@ -47,14 +47,13 @@ create table assets (
   use_years             varchar(20)                           comment '预计使用年限',
   actual_value          varchar(20)                           comment '原值',
   net_value             varchar(20)                           comment '净值',
-  location              varchar(100)                          COMMENT '存放地点',
-  department_id         int(10)                               COMMENT '使用部门',
-  asset_statu_id        int(10)                               COMMENT '使用情况',
-  asset_in_method_id    int(10)                               COMMENT '增加方式',
-  belong_user           varchar(20)                           COMMENT '保管人员',
-  clear_method_id       int(10)                               COMMENT '报废方式',
-  clear_date            DATE                                  COMMENT '报废时间',
-  status                int(1)                                COMMENT '状态：1:正常 2:',
+  location              varchar(100)                          comment '存放地点',
+  department_id         int(10)                               comment '使用部门',
+  asset_status_id       int(10)                               comment '使用情况',
+  asset_in_method_id    int(10)                               comment '增加方式',
+  belong_user           varchar(20)                           comment '保管人员',
+  clear_method_id       int(10)                               comment '报废方式',
+  clear_date            date                                  comment '报废时间',
   created               timestamp                             comment '创建时间',
   modified              timestamp                             comment '修改时间',
   primary key (id)
@@ -62,24 +61,9 @@ create table assets (
 
 
 -- ----------------------------
--- 资产入库
+-- 资产借出
 -- ----------------------------
-create table asset_in (
-  id                    int(10)       not null auto_increment comment '资产入库编号',
-  asset_id              int(10)       not null                comment '资产',
-  in_date               date                                  comment '入库时间',
-  department_id         int(10)                               comment '使用部门',
-  use_person            int(10)                               comment '使用人',
-  created               timestamp                             comment '创建时间',
-  modified              timestamp                             comment '修改时间',
-  primary key  (id)
-)engine=MyISAM default charset=utf8 comment='资产入库';
-
-
--- ----------------------------
--- 资产出库
--- ----------------------------
-create table asset_out (
+create table asset_outs (
   id                    int(10)       not null auto_increment comment '资产入库编号',
   asset_id              int(10)       not null                comment '资产',
   from_date             date                                  comment '借出日期',
@@ -89,7 +73,22 @@ create table asset_out (
   created               timestamp                             comment '创建时间',
   modified              timestamp                             comment '修改时间',
   primary key  (id)
-)engine=MyISAM default charset=utf8 comment='资产入库';
+)engine=MyISAM default charset=utf8 comment='资产借出';
+
+
+-- ----------------------------
+-- 资产归还
+-- ----------------------------
+create table asset_ins (
+  id                    int(10)       not null auto_increment comment '资产入库编号',
+  asset_id              int(10)       not null                comment '资产',
+  in_date               date                                  comment '入库时间',
+  department_id         int(10)                               comment '使用部门',
+  use_person            int(10)                               comment '使用人',
+  created               timestamp                             comment '创建时间',
+  modified              timestamp                             comment '修改时间',
+  primary key  (id)
+)engine=MyISAM default charset=utf8 comment='资产归还';
 
 
 -- ----------------------------

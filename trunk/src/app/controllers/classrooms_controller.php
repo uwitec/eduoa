352dc1b08f4789723aa8f241618fef64bdcc1dd7
longben <longben@cc020657-dd42-0410-9910-adba8de85fba>
2008-01-15 17:08:20 +0000

    完成教室管理阿拉法。

---
+++ trunk/src/app/controllers/classrooms_controller.php
@@ -0,0 +1,128 @@
+<?php
+class ClassroomsController extends AppController {
+
+	var $name = 'Classrooms';
+	var $helpers = array('Html', 'Form' );
+
+	function index() {
+		$this->Classroom->recursive = 0;
+		$this->set('classrooms', $this->Classroom->findAll());
+	}
+
+	function view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash('Invalid id for Classroom.');
+			$this->redirect('/classrooms/index');
+		}
+		$this->set('classroom', $this->Classroom->read(null, $id));
+	}
+
+	function add() {
+		if (empty($this->data)) {
+			$this->set('classroomTypes', 
+				       $this->Classroom->ClassroomType->generateList(
+				         $conditions = null,
+			             $order = 'id',
+			             $limit = null,
+			             $keyPath = '{n}.ClassroomType.id',
+			             $valuePath = '{n}.ClassroomType.type_name')
+			);
+			$this->set('teachingBuildings', 
+				        $this->Classroom->TeachingBuilding->generateList(
+				         $conditions = null,
+			             $order = 'id',
+			             $limit = null,
+			             $keyPath = '{n}.TeachingBuilding.id',
+			             $valuePath = '{n}.TeachingBuilding.building_name')
+		    );
+			$this->render();
+		} else {
+			$this->cleanUpFields();
+			if ($this->Classroom->save($this->data)) {
+				$this->Session->setFlash('教室信息保存成功！');
+				$this->redirect('/classrooms/index');
+			} else {
+				$this->Session->setFlash('Please correct errors below.');
+				$this->set('classroomTypes', 
+						   $this->Classroom->ClassroomType->generateList(
+							 $conditions = null,
+							 $order = 'id',
+							 $limit = null,
+							 $keyPath = '{n}.ClassroomType.id',
+							 $valuePath = '{n}.ClassroomType.type_name')
+				);
+				$this->set('teachingBuildings', 
+							$this->Classroom->TeachingBuilding->generateList(
+							 $conditions = null,
+							 $order = 'id',
+							 $limit = null,
+							 $keyPath = '{n}.TeachingBuilding.id',
+							 $valuePath = '{n}.TeachingBuilding.building_name')
+				);
+			}
+		}
+	}
+
+	function edit($id = null) {
+		if (empty($this->data)) {
+			if (!$id) {
+				$this->Session->setFlash('Invalid id for Classroom');
+				$this->redirect('/classrooms/index');
+			}
+			$this->data = $this->Classroom->read(null, $id);
+			$this->set('classroomTypes', 
+				       $this->Classroom->ClassroomType->generateList(
+				         $conditions = null,
+			             $order = 'id',
+			             $limit = null,
+			             $keyPath = '{n}.ClassroomType.id',
+			             $valuePath = '{n}.ClassroomType.type_name')
+			);
+			$this->set('teachingBuildings', 
+				        $this->Classroom->TeachingBuilding->generateList(
+				         $conditions = null,
+			             $order = 'id',
+			             $limit = null,
+			             $keyPath = '{n}.TeachingBuilding.id',
+			             $valuePath = '{n}.TeachingBuilding.building_name')
+		    );
+		} else {
+			$this->cleanUpFields();
+			if ($this->Classroom->save($this->data)) {
+				$this->Session->setFlash('教室信息修改成功！');
+				$this->redirect('/classrooms/index');
+			} else {
+				$this->Session->setFlash('Please correct errors below.');
+				$this->set('classroomTypes', 
+						   $this->Classroom->ClassroomType->generateList(
+							 $conditions = null,
+							 $order = 'id',
+							 $limit = null,
+							 $keyPath = '{n}.ClassroomType.id',
+							 $valuePath = '{n}.ClassroomType.type_name')
+				);
+				$this->set('teachingBuildings', 
+							$this->Classroom->TeachingBuilding->generateList(
+							 $conditions = null,
+							 $order = 'id',
+							 $limit = null,
+							 $keyPath = '{n}.TeachingBuilding.id',
+							 $valuePath = '{n}.TeachingBuilding.building_name')
+				);
+			}
+		}
+	}
+
+	function delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash('Invalid id for Classroom');
+			$this->redirect('/classrooms/index');
+		}
+		if ($this->Classroom->del($id)) {
+			$this->Session->setFlash('教室删除成功！');
+			$this->redirect('/classrooms/index');
+		}
+	}
+
+}
+?>

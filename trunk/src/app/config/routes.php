c0d5ac6b8b770ba320212a1c2ed22aae38460057
cloudlin <cloudlin@cc020657-dd42-0410-9910-adba8de85fba>
2008-01-22 05:45:08 +0000

    csv 导出demo

--- trunk/src/app/config/routes.php
+++ trunk/src/app/config/routes.php
@@ -3,4 +3,5 @@
 	$Route->connect('/', array('controller' => 'members', 'action' => 'login'));
 	$Route->connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 	$Route->connect('/tests', array('controller' => 'tests', 'action' => 'index'));
+	$Route->connect('/csv/:controller/:action/*', array('webservices' => 'Csv')); 
 ?>

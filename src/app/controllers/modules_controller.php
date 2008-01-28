f991fde96a1fed1c517e622eb7895c525252973a
longben <longben@cc020657-dd42-0410-9910-adba8de85fba>
2008-01-28 06:05:28 +0000

    修改路径

--- src/app/controllers/modules_controller.php
+++ src/app/controllers/modules_controller.php
@@ -1,8 +1,8 @@
-<?php
+﻿<?php
 class ModulesController extends AppController {
 
 	var $name = 'Modules';
-	var $helpers = array('Html', 'Form' );
+	var $helpers = array('Html', 'Form', 'Javascript');
 
 	function index() {
 		$this->Module->recursive = 0;

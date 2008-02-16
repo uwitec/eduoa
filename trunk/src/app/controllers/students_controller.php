1c35c8fcdf00cbd3544ad0195cf99a48e3afb7e8
longben <longben@cc020657-dd42-0410-9910-adba8de85fba>
2008-02-16 14:15:13 +0000

    曹虫QQ提交Bug修改

--- trunk/src/app/controllers/students_controller.php
+++ trunk/src/app/controllers/students_controller.php
@@ -202,13 +202,14 @@ class StudentsController extends AppController {
 						  $sql = "insert into students (banji_id,student_no,student_name,birthday,sex,people_id,native_place,total_score,political_feature,physical_condition,cert_number,graduate_the_college,foreign_language,enter_the_way,origin,address,zip,telphone,email,enter_date,remark,student_phone,father_name,father_unit,father_phone,mother_name,mother_unit,mother_phone)";
 
 						  $sql .= " values ('$banji_id','$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]','$data[6]','$data[7]','$data[8]','$data[9]','$data[10]','$data[11]','$data[12]','$data[13]','$data[14]','$data[15]','$data[16]','$data[17]','$data[18]','$data[19]','$data[20]','$data[21]','$data[22]','$data[23]','$data[24]','$data[25]','$data[26]')";
-						  $sql = iconv("gb2312","UTF-8",$sql);
+
+						  //$sql = iconv("GB2312","UTF-8",$sql);
 						  $this->Student->execute($sql);
 					  }
 					  $count++;
 				  }
 				  $this->Session->setFlash('导入学生档案成功！');
-				  $this->redirect('/students/');
+				  //$this->redirect('/students/');
 				  exit();
 			  }else{
 				$this->set('banjis', 

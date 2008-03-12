33dd969ab3a81600e24b4a82b5e29c7af9b7449e
cloudlin <cloudlin@cc020657-dd42-0410-9910-adba8de85fba>
2008-03-12 02:25:15 +0000

    www update

--- trunk/src/app/webroot/student/bottom.php
+++ trunk/src/app/webroot/student/bottom.php
@@ -1,16 +1,16 @@
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
-  <tr>
-    <td height="8"><img src="images/1_1.gif" width="1" height="1" /></td>
-  </tr>
-</table>
-<table width="854" border="0" align="center" cellpadding="0" cellspacing="0">
-  <tr>
-    <td width="20" background="images/huang_bg.gif"><img src="images/bottom_1.gif" width="20" height="27" /></td>
-    <td width="820" align="center" background="images/huang_bg.gif"><span class="hotfoodfont"><a href="#" class="white">首页</a>&nbsp;| <a href="#" class="white">学校通知</a>&nbsp;| <a href="#" class="white">班级通知</a>&nbsp;| <a href="#" class="white">我的表现</a>&nbsp;| <a href="#" class="white">我的作业</a>&nbsp;| <a href="#" class="white">同步练习</a>&nbsp;| <a href="#" class="white">教辅资料</a>&nbsp;| <a href="#" class="white">考试成绩</a>&nbsp;| <a href="#" class="white">我要开博客</a>&nbsp;| <a href="#" class="white">学校论坛</a></span> </td>
-    <td width="19" background="images/huang_bg.gif"><img src="images/bottom_2.gif" width="20" height="27" /></td>
-  </tr>
-  <tr>
-    <td height="70" colspan="3" align="center">中华人民共和国增值电信业务经营许可证编号：川B2-20040045 　 <br />
-    总机:02883264788 02883260200 客服邮箱：lxy@cdlmkj.com</td>
-  </tr>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td height="8"><img src="images/1_1.gif" width="1" height="1" /></td>
+  </tr>
+</table>
+<table width="854" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="20" background="images/huang_bg.gif"><img src="images/bottom_1.gif" width="20" height="27" /></td>
+    <td width="820" align="center" background="images/huang_bg.gif"><span class="hotfoodfont"><a href="index.php" class="white">首页</a>&nbsp;| <a href="Schoolnotify.php" class="white">学校通知</a>&nbsp;| <a href="Classnotify.php" class="white">班级公告</a>&nbsp;| <a href="MyBehave.php" class="white">我的表现</a>&nbsp;| <a href="MySchoolWork.php" class="white">我的作业</a>&nbsp;| <a href="TbExercise.php" class="white">同步练习</a>&nbsp;| <a href="TutorshipData.php" class="white">教辅资料</a>&nbsp;| <a href="timecard_query.php" class="white">我的考勤</a>&nbsp;| <a href="ExamGrade.php" class="white">考试成绩</a>&nbsp;| <a href="/blog/"   class="white" target="_blank">我的博客</a>&nbsp;| <a href="/bbs/"  class="white" target="_blank">学校论坛</a></span></td>
+    <td width="19" background="images/huang_bg.gif"><img src="images/bottom_2.gif" width="20" height="27" /></td>
+  </tr>
+  <tr>
+    <td height="70" colspan="3" align="center">中华人民共和国增值电信业务经营许可证编号：川B2-20040045 　 <br />
+    总机:02883264788 02883260200 客服邮箱：lxy@cdlmkj.com</td>
+  </tr>
 </table>
--- trunk/src/app/webroot/student/logout.php
+++ trunk/src/app/webroot/student/logout.php
@@ -1,5 +1,6 @@
 <?php
 	setcookie("osStudentNo",'0');
+	session_unregister("osStudentNo");
 	
 	echo("<script language='JavaScript'>location.replace('index.php');</script>");
 ?>
